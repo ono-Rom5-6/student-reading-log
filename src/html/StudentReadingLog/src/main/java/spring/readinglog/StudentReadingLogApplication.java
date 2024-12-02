54a8f12fc347b20a59be0f10a048d6bf3505f0fd
ono-Rom5-6 <atamanoiitori@gmail.com>
2024-12-02 15:26:14 +0900

    Add files via upload

---
+++ src/html/StudentReadingLog/src/main/java/spring/readinglog/StudentReadingLogApplication.java
@@ -0,0 +1,13 @@
+package spring.readinglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentReadingLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentReadingLogApplication.class, args);
+	}
+
+}
